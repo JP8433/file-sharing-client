bca695e606980a1f46979a987c81a6282ad7b615
Jay Patel <68432630+JP8433@users.noreply.github.com>
2023-07-22 20:35:17 +0530

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 // const API_URI = 'http://localhost:8000';
-const API_URI ='https://filesharing-backend-z1r4.onrender.com'
+const API_URI = 'https://filesharing-backend-z1r4.onrender.com'
 
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(`${API_URI}/upload`, data);
+        const response = await axios.post(`${API_URI}/upload`, data,
+            headers: {
+            // 'Content-Type': 'application/x-www-form-urlencoded'
+            'Access-Control-Allow-Origin': '*'
+        });
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
